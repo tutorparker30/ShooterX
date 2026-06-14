9f0b97a93ecf9164b45053fa5d20520f229dfd08
tutorparker30 <tutorparker30@gmail.com>
2026-06-15 00:45:44 +0900

    주요 이벤트 함수와 Movement Component

--- Source/ShooterX/Gimmick/SXHealthPack.cpp
+++ Source/ShooterX/Gimmick/SXHealthPack.cpp
@@ -2,3 +2,55 @@
 
 
 #include "Gimmick/SXHealthPack.h"
+#include "Components/SceneComponent.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/RotatingMovementComponent.h"
+
+ASXHealthPack::ASXHealthPack()
+	: HealAmount(100.f)
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("SceneComponent"));
+	SetRootComponent(SceneComponent);
+
+	BoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComponent"));
+	BoxComponent->SetupAttachment(GetRootComponent());
+	BoxComponent->SetRelativeLocation(FVector(0.f, 0.f, 50.f));
+	BoxComponent->SetRelativeScale3D(FVector(0.6f, 0.8f, 0.8f));
+
+	BodyStaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BodyStaticMeshComponent"));
+	BodyStaticMeshComponent->SetupAttachment(GetRootComponent());
+	BodyStaticMeshComponent->SetRelativeLocation(FVector(0.f, 0.f, 50.f));
+	BodyStaticMeshComponent->SetRelativeRotation(FRotator(0.f, -90.f, 0.f));
+	
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> BodyStaticMesh(TEXT("/Script/Engine.StaticMesh'/Game/LyraResource/Weapons/Healthpack/Mesh/SM_healthpackFull.SM_healthpackFull'"));
+	if (true == BodyStaticMesh.Succeeded())
+	{
+		BodyStaticMeshComponent->SetStaticMesh(BodyStaticMesh.Object);
+	}
+
+	RotatingMovementComponent = CreateDefaultSubobject<URotatingMovementComponent>(TEXT("RotatingMovementComponent"));
+}
+
+void ASXHealthPack::BeginPlay()
+{
+	Super::BeginPlay();
+
+	StartLocation = GetActorLocation();
+
+	RotationSpeed = 300.f;
+	RotatingMovementComponent->RotationRate = FRotator(0.f, RotationSpeed, 0.f);
+}
+
+void ASXHealthPack::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	ElapsedTime += DeltaSeconds;
+	const float ZOffset = FMath::Sin(ElapsedTime * MovementSpeed) * Amplitude;
+	FVector NewLocation = StartLocation;
+	NewLocation.Z += ZOffset;
+	SetActorLocation(NewLocation);
+}
--- Source/ShooterX/Gimmick/SXHealthPack.h
+++ Source/ShooterX/Gimmick/SXHealthPack.h
@@ -6,15 +6,50 @@
 #include "GameFramework/Actor.h"
 #include "SXHealthPack.generated.h"
 
-
+class USceneComponent;
+class UBoxComponent;
+class UStaticMeshComponent;
+class URotatingMovementComponent;
 
 UCLASS()
 class SHOOTERX_API ASXHealthPack : public AActor
 {
 	GENERATED_BODY()
 
+public:
+	ASXHealthPack();
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaSeconds) override;
+
 private:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta=(AllowPrivateAccess), Category = "ShooterX|SXHealthPack")
 	float HealAmount;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta=(AllowPrivateAccess), Category = "ShooterX|SXHealthPack")
+	TObjectPtr<USceneComponent> SceneComponent;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess), Category = "ShooterX|SXHealthPack")
+	TObjectPtr<UBoxComponent> BoxComponent;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess), Category = "ShooterX|SXHealthPack")
+	TObjectPtr<UStaticMeshComponent> BodyStaticMeshComponent;
+
+	FVector StartLocation;
+
+	float ElapsedTime = 0.f;
+
+	UPROPERTY(EditAnywhere, Category = "ShooterX|SXHealthPack")
+	float Amplitude = 10.f;
+
+	UPROPERTY(EditAnywhere, Category = "ShooterX|SXHealthPack")
+	float MovementSpeed = 2.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess), Category = "ShooterX|SXHealthPack")
+	TObjectPtr<URotatingMovementComponent> RotatingMovementComponent;
+
+	UPROPERTY(EditAnywhere, Category = "ShooterX|SXHealthPack")
+	float RotationSpeed = 300.f;
 
 };
